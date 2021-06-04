d995a4671474e2825fad89e6b72f873fa63f3f8b
danda <dan-da@users.noreply.github.com>
2021-06-03 18:38:03 -0700

    chore: export GENESIS_DBC_INPUT so API users can create a Genesis Dbc

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ pub use crate::{
     dbc_transaction::DbcTransaction,
     error::{Error, Result},
     key_manager::{KeyCache, KeyManager, NodeSignature, PublicKey, PublicKeySet, Signature},
-    mint::{Mint, ReissueRequest, ReissueTransaction},
+    mint::{Mint, ReissueRequest, ReissueTransaction, GENESIS_DBC_INPUT},
 };
 
 impl From<[u8; 32]> for Hash {
