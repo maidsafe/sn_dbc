cdbd4ccef72d635227b07cf446cd1bde7c63c14e
danda <dan-da@users.noreply.github.com>
2021-10-01 21:10:29 -0700

    chore: make Amount count/unit private and to_rational() public

--- src/denomination.rs
+++ src/denomination.rs
@@ -116,9 +116,9 @@ impl Denomination {
  
         let mut chosen = vec![];
 
-        let digits = digits(target.count);
+        let digits = digits(target.count());
         let len = digits.len() as PowerOfTen;
-        let exp_base = len - 1 + target.unit;
+        let exp_base = len - 1 + target.unit();
         for (idx, digit) in digits.iter().enumerate() {
             if *digit == 0 {
                 continue;
@@ -145,9 +145,9 @@ impl TryFrom<Amount> for Denomination {
     fn try_from(amt: Amount) -> Result<Self> {
 
         let a = amt.to_highest_unit();
-        let exp = a.unit;
+        let exp = a.unit();
        
-        let denom = match a.count {
+        let denom = match a.count() {
             1 => Self::One(exp),
             2 => Self::Two(exp),
             3 => Self::Three(exp),
