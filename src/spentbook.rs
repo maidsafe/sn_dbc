8997846def48ad11c4894c8d9f7eaae492bdbdee
danda <dan-da@users.noreply.github.com>
2022-02-06 12:08:46 -0800

    improvement: make spentbook more efficient
    change the way data is stored and queried in SpentBookNodeMock
    
    * store only a single copy of each Tx. tx_hash    -> tx
    * store key_image index.               key_image  -> tx_hash
    * store public_key index:              public_key -> output_proof
    * avoid full table scan when matching input pks with output pk.
    * add some explanation in doc comment

--- src/spentbook.rs
+++ src/spentbook.rs
@@ -1,7 +1,7 @@
 use blst_ringct::ringct::{OutputProof, RingCtMaterial, RingCtTransaction};
 use blst_ringct::DecoyInput;
 use blstrs::group::Curve;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 
 use rand8::prelude::IteratorRandom;
 
@@ -10,17 +10,35 @@ use crate::{Commitment, Hash, KeyImage, KeyManager, Result, SimpleKeyManager, Sp
 /// This is a mock SpentBook used for our test cases. A proper implementation
 /// will be distributed, persistent, and auditable.
 ///
-/// This impl has a serious inefficiency when looking up OutputProofs by
-/// PublicKey.  A scan of all spent Tx is required.  This is not a problem
-/// for small tests.
+/// This impl attempts to be reasonably efficient.  In particular
+/// it stores only a single copy of each Tx and includes indexes:
+///     tx_hash    --> Tx
+///     key_image  --> tx_hash
+///     public_key --> OutputProof
 ///
-/// A real (performant) impl would need to add an additional index/map from
-/// PublicKey to Tx.  Or alternatively from PublicKey to KeyImage.  This requirement
-/// may add complexity to a distributed implementation.
+/// The public_key map eliminates a full table scan when matching
+/// public keys for each input of logged Tx to public key of OutputProof in
+/// already-spent Txs.
+///
+/// This impl does duplicate the OutputProofs in the public_key index, which
+/// is not ideal and should not be done for a "real" system.
+/// 
+/// Another approach would be to map public_key --> tx_hash. This eliminates
+/// the need to store duplicate OutputProof. One could lookup the Tx with
+/// the desired OutputProof, and then iterate through outputs to actually find it.
+///
+/// See the very first commit of this file For a naive impl that uses only
+/// a single map<key_image, tx>.
 #[derive(Debug, Clone)]
 pub struct SpentBookNodeMock {
     pub key_manager: SimpleKeyManager,
-    pub transactions: BTreeMap<KeyImage, RingCtTransaction>,
+
+    pub transactions: HashMap<Hash, RingCtTransaction>,
+    pub key_images: BTreeMap<KeyImage, Hash>,
+
+    // fixme: key is really a PublicKeyBlst (G1Affine). We should make a KeyImage
+    pub outputs: BTreeMap<KeyImage, OutputProof>,
+
     pub genesis: Option<(KeyImage, Commitment)>, // genesis input (keyimage, public_commitment)
 }
 
@@ -29,6 +47,8 @@ impl From<SimpleKeyManager> for SpentBookNodeMock {
         Self {
             key_manager,
             transactions: Default::default(),
+            key_images: Default::default(),
+            outputs: Default::default(),
             genesis: None,
         }
     }
@@ -41,18 +61,23 @@ impl From<(SimpleKeyManager, KeyImage, Commitment)> for SpentBookNodeMock {
         Self {
             key_manager,
             transactions: Default::default(),
+            key_images: Default::default(),
+            outputs: Default::default(),
             genesis: Some((key_image, public_commitment)),
         }
     }
 }
 
 impl SpentBookNodeMock {
-    pub fn iter(&self) -> impl Iterator<Item = (&KeyImage, &RingCtTransaction)> {
-        self.transactions.iter()
+    pub fn iter(&self) -> impl Iterator<Item = (&KeyImage, &RingCtTransaction)> + '_ {
+        // fixme: unwrap.  maybe try_iter() instead?
+        self.key_images
+            .iter()
+            .map(move |(k, h)| (k, self.transactions.get(h).unwrap()))
     }
 
     pub fn is_spent(&self, key_image: &KeyImage) -> bool {
-        self.transactions.contains_key(key_image)
+        self.key_images.contains_key(key_image)
     }
 
     pub fn log_spent(
@@ -98,51 +123,44 @@ impl SpentBookNodeMock {
             if key_image == *genesis_key_image {
                 vec![(key_image.clone(), vec![*genesis_public_commitment])]
             } else {
-                // Todo: make this cleaner and more efficient.
-                //       spentbook needs to also be indexed by OutputProof PublicKey.
-                //       perhaps map PublicKey --> KeyImage.
                 tx.mlsags
                     .iter()
                     .map(|mlsag| {
-                        let commitments: Vec<Commitment> = mlsag
+                        // For each public key in ring, look up matching OutputProof
+                        // note: We use flat_map to avoid get.unwrap()
+                        let output_proofs: Vec<&OutputProof> = mlsag
                             .public_keys()
                             .iter()
-                            .map(|pk| {
-                                let output_proofs: Vec<&OutputProof> = self
-                                    .transactions
-                                    .values()
-                                    .filter_map(|ringct_tx| {
-                                        ringct_tx
-                                            .outputs
-                                            .iter()
-                                            .find(|proof| proof.public_key() == pk)
-                                    })
-                                    .collect();
-
-                                // note: all inputs to a tx will store the same Tx.  As such,
-                                // we will can get multiple matches.  But they should/must
-                                // be from the same Tx.  So we use only the first one.
-                                // A better impl would store only a single Tx with multiple
-                                // KeyImage pointers to it.
-
-                                assert!(!output_proofs.is_empty());
-                                output_proofs[0].commitment()
+                            .flat_map(|pk| {
+                                // fixme: we (ab)use KeyImage G1Affine wrapper to give us Ord trait.
+                                let pki: KeyImage = (*pk).into();
+                                self.outputs.get(&pki)
                             })
                             .collect();
+
+                        // collect commitments from OutputProofs
+                        let commitments: Vec<Commitment> =
+                            output_proofs.iter().map(|o| o.commitment()).collect();
+
+                        // check our assumptions.
                         assert_eq!(commitments.len(), mlsag.public_keys().len());
                         assert!(commitments.len() == mlsag.ring.len());
+
                         (mlsag.key_image.into(), commitments)
                     })
                     .collect()
             };
 
-        // Grab the commitments specific to the spent KeyImage
+        // Grab all commitments, grouped by input mlsag
+        // Needed for Tx verification.
         let tx_public_commitments: Vec<Vec<Commitment>> = public_commitments_info
             .clone()
             .into_iter()
             .map(|(_, v)| v)
             .collect();
 
+        // Grab the commitments specific to the input KeyImage
+        // Needed for SpentProofShare
         let public_commitments: Vec<Commitment> = public_commitments_info
             .into_iter()
             .flat_map(|(k, v)| if k == key_image { v } else { vec![] })
@@ -153,11 +171,23 @@ impl SpentBookNodeMock {
             tx.verify(&tx_public_commitments)?;
         }
 
-        let existing_tx = self
-            .transactions
+        // Add key_image:tx_hash to key_image index.
+        let existing_tx_hash = self
+            .key_images
             .entry(key_image.clone())
-            .or_insert_with(|| tx.clone());
-        if existing_tx.hash() == tx.hash() {
+            .or_insert_with(|| tx_hash);
+
+        if *existing_tx_hash == tx_hash {
+            // Add tx_hash:tx to transaction entries. (primary data store)
+            let existing_tx = self.transactions.entry(tx_hash).or_insert_with(|| tx);
+
+            // Add public_key:output_proof to public_key index.
+            // fixme: we (ab)use KeyImage G1Affine wrapper to give us Ord trait.
+            for output in existing_tx.outputs.iter() {
+                let pki: KeyImage = (*output.public_key()).into();
+                self.outputs.entry(pki).or_insert_with(|| output.clone());
+            }
+
             Ok(SpentProofShare {
                 key_image,
                 spentbook_pks,
@@ -165,6 +195,7 @@ impl SpentBookNodeMock {
                 public_commitments,
             })
         } else {
+            // fixme: return an error.
             panic!("Attempt to Double Spend")
         }
     }
